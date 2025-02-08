7b94219a88552a8b4f40b3d96447f7d12079464d
Sandun Rathsara <srathsara1@gmail.com>
2025-02-08 12:45:35 +0000

    SETTING UP setup.sh | apt packages ✅, yazi ✅, minor upgrades

--- setup.sh
+++ setup.sh
@@ -1,26 +1,48 @@
 #!/usr/bin/zsh
 
-# declar -a apt_packs=(
-#   build-essential stow zsh
-# )
+# Install apt packages
+apt_install() {
+  sudo apt install build-essential stow zsh unzip tmux
+}
 
+# Inspall yazi packages
 snap_install() {
   sudo snap install --classic bash-language-server
   sudo snap install --classic nvim
 }
 
-main() {
-  # install apt packs
-  snap_install
+# Special function to install yazi
+install_yazi() {
+  wget -qO ~/downloads/yazi.zip https://github.com/sxyazi/yazi/releases/latest/download/yazi-x86_64-unknown-linux-gnu.zip
+  unzip -q ~/downloads/yazi.zip -d ~/downloads/yazi-temp
+  sudo mv ~/downloads/yazi-temp/*/yazi /usr/local/bin
+}
 
+# Special function to setup tpm for tmux
+setup_tpm() {
+  # clone the tpm repo
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  # install the plugins
+  ~/.tmux/plugins/tpm/bin/install_plugins
+}
+
+main() {
   # Simlink farm
   stow .
 
-  #=== Setup tmux config with tpm
-  # clone the tpm repo
-  # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-  # install the plugins
-  # ~/.tmux/plugins/tpm/bin/install_plugins
+  # Create downloads folder
+  mkdir -p ~/downloads
+
+  # Install apt packages
+  apt_install
+  # Install snap packages
+  snap_install
+
+  # Install yazi
+  install_yazi
+
+  # Setup tmux
+  setup_tpm
 }
 
 main "$@"
