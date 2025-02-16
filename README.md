a058d58fec229c66a3d167666f5fc83cc497baf2
Ubuntu <ubuntu@srv662878.hstgr.cloud>
2025-02-16 08:12:11 +0000

    SETTING UP setup.sh | DIGIS VPS set up using the new config

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # Initial setup
 
-clone the repo
+## SSH Hardening
+Change the bellow values in the respective files
 
+### 1. sshd_config
+```sh
+sudo vim /etc/ssh/sshd_config
+```
+PasswordAuthentication no
+PermitRootLogin no
+UsePAM no
+
+### 2. sshd_config.d/50-cloud-init.conf
+```sh
+sudo vim /etc/ssh/sshd_config.d/50-cloud-init.conf
+```
+PasswordAuthentication no
+
+## Run setup script
+
+### 1. Clone the repo
 ```sh
 git clone https://SandunRathsara@github.com/SandunRathsara/vps-dotfiles.git ~/.dotfiles
 ```
 
+### 2. CD into dotfiles directory
+```sh
+cd ~/.dotfiles
+```
+### 3. Give execution permission to the setup file
+```sh
+sudo chmod +x ./setup.sh
+```
+### 4. Run the setup file
+```sh
+./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 
 # Install apt packages
 apt_install() {
@@ -11,11 +11,24 @@ snap_install() {
   sudo snap install --classic nvim
 }
 
+# Manual setup of powerlevel10k shell
+setup_powerlevel10k() {
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+}
+
 # Special function to install yazi
 install_yazi() {
   wget -qO ~/downloads/yazi.zip https://github.com/sxyazi/yazi/releases/latest/download/yazi-x86_64-unknown-linux-gnu.zip
   unzip -q ~/downloads/yazi.zip -d ~/downloads/yazi-temp
   sudo mv ~/downloads/yazi-temp/*/yazi /usr/local/bin
+}
+
+# Install lazygit
+install_lazygit() {
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo ~/downloads/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf ~/downloads/lazygit.tar.gz ~/downloads/lazygit
+  sudo install ~/downloads/lazygit -D -t /usr/local/bin/
 }
 
 # Special function to setup tpm for tmux
@@ -27,9 +40,6 @@ setup_tpm() {
 }
 
 main() {
-  # Simlink farm
-  stow .
-
   # Create downloads folder
   mkdir -p ~/downloads
 
@@ -38,11 +48,23 @@ main() {
   # Install snap packages
   snap_install
 
+  # Simlink farm
+  stow .
+
+  # Setup powerlevel10k
+  setup_powerlevel10k
+
   # Install yazi
   install_yazi
 
+  # Install lazygit
+  install_lazygit
+
   # Setup tmux
   setup_tpm
+
+  # Change the default shell
+  chsh -s /usr/bin/zsh
 }
 
 main "$@"
